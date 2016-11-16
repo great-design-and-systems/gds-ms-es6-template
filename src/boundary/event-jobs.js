d33558207e80a2da3fe01a9b05f68352c3c5cd9b
rickzx98 <rickzx98@gmail.com>
2016-11-16 11:03:54 +0800

    updates

--- src/boundary/event-jobs.js
+++ src/boundary/event-jobs.js
@@ -1,10 +1,23 @@
 import CreateEventJob from '../control/event-job/create-event-job';
 import RemoveEventJobById from '../control/event-job/remove-event-job-by-id';
 import GetEventByName from '../control/event/get-event-by-name';
+
 export default class EventJobService {
     createEventJob(eventName, context, callback) {
         new GetEventByName(eventName, (err, event) => {
             console.log('event', event);
+            switch (event.eventType) {
+                case 'PROCESS':
+                    runProcessEvent(event, context, callback);
+                    break;
+            }
         });
     }
 }
+
+function runProcessEvent(event, context, callback) {
+    new CreateEventJob(event.name, context.session, 'NEW', (err, processJob) => {
+
+    });
+}
+
